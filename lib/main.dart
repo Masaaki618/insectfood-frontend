758496d40fabf91a1e5851164b6cf7fd9e87c7d0
Masaaki618 <masaaki_618@icloud.com>
2026-04-11 21:21:11 +0900

    修正: .env.example をアセットに追加して dotenv.load() で使用

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:frontend/shared/api/api_client.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env.example");
   ApiClient.initialize();
   runApp(const ProviderScope(child: App()));
 }
